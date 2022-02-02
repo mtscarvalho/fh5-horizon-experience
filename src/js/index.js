e693a23e2b9d942f7abefdb7420c293098919b01
Matheus Carvalho <henriquematheus.c@gmail.com>
2022-02-01 21:26:44 -0300

    Add FAQ accordion

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,45 @@
-console.log("It's working!")
+/**
+ * Accordion
+ */
+
+function initAccordion() {
+  const
+    accordionButton = document.querySelectorAll(".accordion-button"),
+    accordionContent = document.querySelectorAll(".accordion-content");
+
+  function toggleAccordion() {
+    closeOthers(this);
+  
+    if(this.getAttribute("aria-expanded") === "false") {
+      this.setAttribute("aria-expanded", "true");
+      this.nextElementSibling.setAttribute("aria-hidden", "false");
+    } else {
+      this.setAttribute("aria-expanded", "false");
+      this.nextElementSibling.setAttribute("aria-hidden", "true");
+    }
+  }
+  
+  function closeOthers(target) {
+    accordionButton.forEach(item => {
+      if(item != target) {
+        item.setAttribute("aria-expanded", "false");
+        item.nextElementSibling.setAttribute("aria-hidden", "true");
+      }
+    });
+  }
+
+  for (let i = 0; i < accordionContent.length; i++) {
+    accordionContent[i].setAttribute("aria-hidden", "true");
+    accordionContent[i].setAttribute("aria-labelledby", `accordion-id-${i}`);
+    accordionContent[i].setAttribute("id", `accordion-sect-${i}`);
+  }
+
+  for (let i = 0; i < accordionButton.length; i++) {
+    accordionButton[i].setAttribute("aria-expanded", "false");
+    accordionButton[i].setAttribute("aria-controls", `accordion-sect-${i}`);
+    accordionButton[i].setAttribute("id", `accordion-id-${i}`);
+    accordionButton[i].addEventListener("click", toggleAccordion);
+  }
+}
+
+initAccordion()
